fda3cf559de3e44544d6c64aba955d2c9108e6e6
Priyanshu <priyanshumahawar29@gmail.com>
2023-02-09 14:24:57 +0530

    feedback Screen

---
+++ lib/screens/feedbackscreen.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:kriti/components/bottom_nav_bar.dart';
+
+class FeedbackScreen extends StatefulWidget {
+  const FeedbackScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FeedbackScreen> createState() => _FeedbackScreenState();
+}
+
+class _FeedbackScreenState extends State<FeedbackScreen> {
+
+  int _currentButton = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          'assets/images/bgImage1.png',
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SizedBox(
+            width:MediaQuery.of(context).size.width ,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 55.0),
+                  child: Container(
+                    width: 200.0,
+                    height: 200.0,
+                    // padding: const EdgeInsets.only(bottom: 1000) ,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+                    'How was the service of\n your order from this\n shop?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      // fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+                    'Rate the shop,your feedback matters',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      // fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Color.fromRGBO(114, 114, 114, 1.0),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: Icon(_currentButton < 1 ? Icons.star_border_rounded : Icons.star_rate_rounded),
+                          onPressed:(){
+                            setState(() {
+                              _currentButton = 1;
+                            });
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(_currentButton < 2 ? Icons.star_border_rounded : Icons.star_rate_rounded),
+                          onPressed:(){
+                            setState(() {
+                              _currentButton = 2;
+                            });
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(_currentButton < 3 ? Icons.star_border_rounded : Icons.star_rate_rounded),
+                          onPressed:(){
+                            setState(() {
+                              _currentButton = 3;
+                            });
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(_currentButton < 4 ? Icons.star_border_rounded : Icons.star_rate_rounded),
+                          onPressed:(){
+                            setState(() {
+                              _currentButton = 4;
+                            });
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(_currentButton < 5 ? Icons.star_border_rounded : Icons.star_rate_rounded),
+                          onPressed:(){
+                            setState(() {
+                              _currentButton = 5;
+                            });
+                          },
+                        )
+                      ]
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              )
+                          ),
+                          backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(188, 157, 255, 1.0)),
+                        ),
+                        onPressed: (){},
+                        child: const Text('No, Thanks')
+                    ),
+                    ElevatedButton(
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              )
+                          ),
+                          backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(188, 157, 255, 1.0)),
+                        ),
+                        onPressed: (){},
+                        child: const Text('Submit')
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          bottomNavigationBar: const BottomNavBar(),
+        ),
+      ],
+
+    );
+  }
+}
