2fcb0e0ca6cc2ed4c29d6fa333c269e4bb889f3e
tarunnn3301 <k.tarun@iitg.ac.in>
2023-02-12 21:41:23 +0530

    Order Page added

--- lib/screens/orderscreen.dart
+++ lib/screens/orderscreen.dart
@@ -15,6 +15,12 @@ class _OrderPageState extends State<OrderPage> {
     ["fhjh", "sflgjk"],
     ["jhg"]
   ];
+  List GandList = [
+    ["aloo seez", "spersh", "mahaveer"],
+    ["duck", "kabootar"],
+    ["fhjh", "sflgjk"],
+    ["jhg"]
+  ];
   var selected = 1;
   var size, height, width;
 
@@ -151,6 +157,11 @@ class _OrderPageState extends State<OrderPage> {
                   )
                       : Container(
                     height: 550,
+                    child: ListView.builder(
+                        itemCount: GandList.length,
+                        itemBuilder: (context, index) {
+                          return customer_order_active_card(items: GandList[index]);
+                        }),
                   ),
                 ],
               ),
@@ -256,8 +267,15 @@ class OrderCard extends StatefulWidget {
   @override
   State<OrderCard> createState() => _OrderCardState();
 }
+class customer_order_active_card extends StatefulWidget {
+  final List<String> items;
 
-class _OrderCardState extends State<OrderCard> {
+  customer_order_active_card({this.items = const []});
+
+  @override
+  State<customer_order_active_card> createState() => _CustomerOrderActiveCard();
+}
+class _CustomerOrderActiveCard extends State<customer_order_active_card> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -265,7 +283,7 @@ class _OrderCardState extends State<OrderCard> {
       child: Container(
         margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
         // width: 280,
-        height: widget.items.length * 25 + 145,
+        height: widget.items.length*22+150,
         // decoration: BoxDecoration(
         //   color: Color.fromRGBO(255, 249, 240, 1.0),
         //   borderRadius: BorderRadius.circular(15),
@@ -282,38 +300,185 @@ class _OrderCardState extends State<OrderCard> {
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(15),
                     topRight: Radius.circular(15),
-                  )),
+                  )
+              ),
               child: Text(
                 'Kapili Canteen',
                 style: TextStyle(
                     fontSize: 25,
                     color: Color.fromRGBO(255, 255, 255, 1),
                     fontWeight: FontWeight.bold,
-                    fontFamily: 'DMSans'),
+                    fontFamily: 'DMSans'
+                ),
               ),
+
             ),
             Container(
               color: Color.fromRGBO(255, 249, 240, 1.0),
-              padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-              height: widget.items.length * 25 + 35,
+              padding: EdgeInsets.fromLTRB(20, 0,
+                  20, 0),
+              height: widget.items.length*22+40,
               // height: items.length == 1 ? 60 : items.length*33,
               width: 280,
               child: Column(
                 children: <Widget>[
-                  SizedBox(
-                    height: 20,
-                  ),
+                  SizedBox(height: 20,),
                   Expanded(
                     child: ListView.builder(
                       itemCount: widget.items.length,
-                      itemBuilder: (context, index) {
-                        return Text(
-                          widget.items[index],
-                          style: TextStyle(fontSize: 18, fontFamily: 'DMSans'),
+                      itemBuilder: (context,index){
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(widget.items[index], style: TextStyle(fontSize: 18,fontFamily: 'DMSans'),),
+                            Text("x1", style: TextStyle(fontSize: 16,fontFamily: 'DMSans',color: Color.fromRGBO(114, 114, 114, 1.0)),),
+                          ],
                         );
                       },
                     ),
+                  ),
+                  Align(
+                    heightFactor: 1.3,
+                    alignment: Alignment.topRight,
+                    child: Text("Rs. 100", style: TextStyle(fontSize: 16,fontFamily: 'DMSans',color: Color.fromRGBO(114, 114, 114, 1.0)),),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              // margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                height: 55,
+                width: 280,
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 249, 240, 1.0),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(15),
+                        bottomRight: Radius.circular(15)
+                    )
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: const ImageIcon(AssetImage('assets/icons/qrcode.png'), color: Colors.transparent,),
+                          iconSize: 0,
+                          onPressed: (){},
+                          // color: Colors.red,
+                        ),
+                        Container(
+                          width: 120,
+                          // padding: EdgeInsets.fromLTRB(73, 0, 0, 7),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                shape: StadiumBorder(),
+                                primary: Color.fromRGBO(188, 157, 255, 1),
+                                onPrimary: Color.fromRGBO(255, 255, 255, 1.0)
+                            ),
+                            child: Text(
+                              'Show Status',
+                              style: TextStyle(
+                                fontFamily: 'DMSans',
+                                fontSize: 15,
+                              ),
+                            ),
+                            onPressed: (){},
+                          ),
+                        ),
+                        IconButton(
+                          icon: ImageIcon(AssetImage('assets/icons/qrcode.png')),
+                          iconSize: 30,
+                          onPressed: (){},
+                          // color: Colors.red,
+                        )
+
+                      ]
+                  ),
+                )
+
+            )
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+class _OrderCardState extends State<OrderCard>{
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
+        // width: 280,
+        height: widget.items.length*22+150,
+        // decoration: BoxDecoration(
+        //   color: Color.fromRGBO(255, 249, 240, 1.0),
+        //   borderRadius: BorderRadius.circular(15),
+        // ),
+        child: Column(
+          children: [
+            Container(
+              //padding: EdgeInsets.fromLTRB(20, 0, right, bottom),
+              height: 55,
+              width: 280,
+              padding: EdgeInsets.fromLTRB(20, 15, 0, 0),
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(188, 157, 255, 1.0),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
                   )
+              ),
+              child: Text(
+                'Kapili Canteen',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'DMSans'
+                ),
+              ),
+
+            ),
+            Container(
+              color: Color.fromRGBO(255, 249, 240, 1.0),
+              padding: EdgeInsets.fromLTRB(20, 0,
+                  20, 0),
+              height: widget.items.length*22+40,
+              // height: items.length == 1 ? 60 : items.length*33,
+              width: 280,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 20,),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: widget.items.length,
+                      itemBuilder: (context,index){
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(widget.items[index], style: TextStyle(fontSize: 18,fontFamily: 'DMSans'),),
+                            Text("x1",style: TextStyle(color: Color.fromRGBO(114, 114, 114, 1.0),fontSize: 16))
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  Align(
+                    heightFactor: 1.3,
+                    alignment: Alignment.topRight,
+                    child: Text("Rs. 100", style: TextStyle(fontSize: 16,fontFamily: 'DMSans',color: Color.fromRGBO(
+                        114, 114, 114, 1.0)),),
+                  ),
                 ],
               ),
             ),
@@ -324,51 +489,65 @@ class _OrderCardState extends State<OrderCard> {
                     color: Color.fromRGBO(255, 249, 240, 1.0),
                     borderRadius: BorderRadius.only(
                         bottomLeft: Radius.circular(15),
-                        bottomRight: Radius.circular(15))),
-                child: Row(children: [
-                  Container(
-                    width: 140,
-                    padding: EdgeInsets.fromLTRB(20, 0, 20, 7),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          elevation: 0,
-                          shape: StadiumBorder(),
-                          primary: Color.fromRGBO(188, 157, 255, 1),
-                          onPrimary: Color.fromRGBO(255, 255, 255, 1.0)),
-                      child: Text(
-                        'Review',
-                        style: TextStyle(
-                          fontFamily: 'DMSans',
-                          fontSize: 15,
+                        bottomRight: Radius.circular(15)
+                    )
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 90,
+                          // padding: EdgeInsets.fromLTRB(20, 0, 20, 7),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                shape: StadiumBorder(),
+                                primary: Color.fromRGBO(188, 157, 255, 1),
+                                onPrimary: Color.fromRGBO(255, 255, 255, 1.0)
+                            ),
+                            child: Text(
+                              'Reorder',
+                              style: TextStyle(
+                                fontFamily: 'DMSans',
+                                fontSize: 15,
+                              ),
+                            ),
+                            onPressed: (){},
+                          ),
                         ),
-                      ),
-                      onPressed: () {},
-                    ),
+                        Container(
+                          width: 90,
+                          // padding: EdgeInsets.fromLTRB(20, 0, 20, 7),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                shape: StadiumBorder(),
+                                primary: Color.fromRGBO(188, 157, 255, 1),
+                                onPrimary: Color.fromRGBO(255, 255, 255, 1.0)
+                            ),
+                            child: Text(
+                              'Review',
+                              style: TextStyle(
+                                fontFamily: 'DMSans',
+                                fontSize: 15,
+                              ),
+                            ),
+                            onPressed: (){},
+                          ),
+                        )
+
+                      ]
                   ),
-                  Container(
-                    width: 140,
-                    padding: EdgeInsets.fromLTRB(20, 0, 20, 7),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          elevation: 0,
-                          shape: StadiumBorder(),
-                          primary: Color.fromRGBO(188, 157, 255, 1),
-                          onPrimary: Color.fromRGBO(255, 255, 255, 1.0)),
-                      child: Text(
-                        'Reorder',
-                        style: TextStyle(
-                          fontFamily: 'DMSans',
-                          fontSize: 15,
-                        ),
-                      ),
-                      onPressed: () {},
-                    ),
-                  )
-                ]))
+                )
+
+            )
+
+
           ],
         ),
       ),
     );
   }
 }
-
