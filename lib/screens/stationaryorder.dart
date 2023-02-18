7b3a74d24f0473bd8c35047de87d23484cf9a120
sparshmittal <sparshmittal2311@gmail.com>
2023-02-18 21:58:12 +0530

    stationary accept reject done

--- lib/screens/stationaryorder.dart
+++ lib/screens/stationaryorder.dart
@@ -472,10 +472,9 @@ class _StationaryActiveCard extends State<StationaryActiveCard>{
       if (!mounted) return;
       setState(() {
         getResult = qrCode;
-        if(getResult==widget.items['Order_Key']){
+        if(getResult==widget.items['Order_Key']) {
           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text(
-                  "Order completed.")));
+              content: Text("Order completed.")));}
           db.OrderDelete(widget.items['Order_Key']);
       });
     }catch(e){
